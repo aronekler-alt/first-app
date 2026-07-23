7634bde15dfc1b844bf4ac2e42a2a25e1fc3ed31
aronekler-alt <aron.ekler@gmail.com>
2026-07-23 10:44:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import streamlit as st
-
-st.title("Az első weboldalam")
-st.write("Üdv Áron weboldalán!")
-
-nev = st.text_input("Hogy hívnak?")
-if nev:
-    st.write("Szia, " + nev + "! Jó, hogy itt vagy.")
-
-szam = st.slider("Válassz számot:", 0, 10000)
-st.write("A szam duplaja: ", szam*2)
-
-szam1 = st.number_input("Szám1: ")
-szam2 = st.number_input("Szám2: ")
-st.write("A szamok összege: ", szam1+szam2)
+class Allat:
+    def __init__(self, nev):
+        self.nev = nev
+    def hang(self): 
+        return "..."
+class Kutya(Allat):
+    def hang(self):
+        return "Vau!"
+class Macska(Allat):
+    def hang(self):
+        return "Miau!"
+st.title("Kis állatkert")
+if "allatok" not in st.session_state:
+    st.session_state.allatok = []
+nev = st.text_input("Az állat neve")
+faj = st.radio("Milyen állat?", ["Kutya", "Macska"])
+if st.button("Hozzáadás"):
+    if faj == "Kutya":
+        st.session_state.allatok.append(Kutya(nev))
+else:
+    st.session_state.allatok.append(Macska(nev))
+st.write("Az állatkert lakói:")
+for allat in st.session_state.allatok:
+    st.subheader(allat.nev)
+st.write("Hangja: " + allat.hang())
+st.metric("Állatok száma", len(st.session_state.allatok))
